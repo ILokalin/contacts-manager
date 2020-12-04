1d225a5d648d94b9a31bf80ff28916e87ab0b875
Ilya Lokalin <ilya-1974@yandex.ru>
2020-12-04 15:46:26 +0500

    feat: read contacts after authorization

--- src/pages/ContactsPage/index.jsx
+++ src/pages/ContactsPage/index.jsx
@@ -1,11 +1,22 @@
+import { useEffect, useState } from "react";
 import { AddContactForm } from "components/AddContactForm";
 import { ContactsContainer } from "components/ContactsContainer";
+import { loadContactsByUser } from "redux/actions";
+import { useSelector, useDispatch } from "react-redux";
 
 export const ContactsPage = () => {
-  const contacts = [
-    { name: "Pakemon Sloun", phone: "79122222212", id: "gh-234" },
-    { name: "Pakemon Stoun", phone: "79122200212", id: "gh-235" },
-  ];
+  const dispatch = useDispatch();
+  const user = useSelector(({ auth }) => auth.user);
+  const [contacts, setContacts] = useState([]);
+
+  useEffect(() => {
+    dispatch(loadContactsByUser(user));
+  }, []);
+
+  // const contacts = [
+  //   { name: "Pakemon Sloun", phone: "79122222212", id: "gh-234" },
+  //   { name: "Pakemon Stoun", phone: "79122200212", id: "gh-235" },
+  // ];
 
   return (
     <div className="row">
