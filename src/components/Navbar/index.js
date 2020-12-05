e9c8cc56137326bb4f7e48c02ba5d091d318b626
ILokalin <51790923+ILokalin@users.noreply.github.com>
2020-12-06 04:35:16 +0500

    feat: show user name on the navbar

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,12 +1,25 @@
+import { useDispatch } from "react-redux";
 import { SearchForm } from "components/SearchForm";
+import { useSelector } from "react-redux";
+import { userLogout } from "redux/actions";
 
 export const Navbar = () => {
+  const dispatch = useDispatch();
+  const userName = useSelector(({ auth }) => auth.name);
+
+  const onLogoutClick = () => {
+    dispatch(userLogout());
+  };
   return (
     <nav className="navbar navbar-light bg-light">
       <div className="container">
-        <span className="navbar-brand">Contacts Manager</span>
+        <span className="navbar-brand">{userName}</span>
         <SearchForm />
-        <button className="btn btn-link my-2 my-sm-0" type="button">
+        <button
+          className="btn btn-link my-2 my-sm-0"
+          type="button"
+          onClick={onLogoutClick}
+        >
           Logout
         </button>
       </div>
