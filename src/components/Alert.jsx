3923cc0747c23ed5f5db565a05189bad5b91e513
ILokalin <51790923+ILokalin@users.noreply.github.com>
2020-12-06 17:08:56 +0500

    feat: correct alert's postion to center

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -1,12 +1,22 @@
 import React from "react";
+import { useDispatch } from "react-redux";
+import { removeAlert } from "redux/actions";
 
 const style = {
   alert: {
     position: "absolute",
+    left: "50%",
+    transform: "translateX(-50%)",
   },
 };
 
 export const Alert = ({ message }) => {
+  const dispatch = useDispatch();
+
+  const onCloseButtonClick = () => {
+    dispatch(removeAlert());
+  };
+
   return (
     <div
       style={style.alert}
@@ -20,6 +30,7 @@ export const Alert = ({ message }) => {
         className="close"
         data-dismiss="alert"
         aria-label="Close"
+        onClick={onCloseButtonClick}
       >
         <span aria-hidden="true">&times;</span>
       </button>
