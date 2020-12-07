381a4071ceb79d5327364ec4cb990ae7b3719822
Ilya Lokalin <ilya-1974@yandex.ru>
2020-12-07 17:22:22 +0500

    feat: update json-server and add proxy for optimize run dev

--- src/utils/http/http.js
+++ src/utils/http/http.js
@@ -1,11 +1,9 @@
 import axios from "axios";
 import { isSuccess } from "utils";
 
-const URL = "http://localhost:4000/";
-
 export const http = async (urn, method = "get", data = null) => {
   try {
-    const response = await axios[method](URL + urn, data);
+    const response = await axios[method](urn, data);
     isSuccess(response);
     return response;
   } catch (e) {
