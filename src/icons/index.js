d12264d77b43d5add0cc246f7a48c372ead14b5b
Ilya Lokalin <ilya-1974@yandex.ru>
2020-12-11 16:44:48 +0500

    feat: create icon

--- src/icons/index.js
+++ src/icons/index.js
@@ -1,5 +1,6 @@
 export { AccountIcon } from "./AccountIcon";
 export { CheckIcon } from "./CheckIcon";
+export { DotsIcon } from "./DotsIcon";
 export { PencilIcon } from "./PencilIcon";
 export { RemoveIcon } from "./RemoveIcon";
 export { SearchIcon } from "./SearchIcon";
